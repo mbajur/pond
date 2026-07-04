071c85c0afd7940efab7d511d99c8539d7c93be4
mbajur <mbajur@gmail.com>
2026-07-04 21:47:37 +0200

    All the other handlers specs

--- app/services/url_thumbnailer/youtube_video_handler.rb
+++ app/services/url_thumbnailer/youtube_video_handler.rb
@@ -2,7 +2,14 @@ class UrlThumbnailer::YoutubeVideoHandler < UrlThumbnailer::DefaultHandler
   # Maybe regexp would be more elegant?
   def self.match?(post:, preflight:)
     uri = URI.parse(post.url)
-    uri.host&.include?("youtube.com") && uri.path == "/watch" && uri.query&.include?("v=")
+
+    if uri.host&.include?("youtube.com")
+      uri.path == "/watch" && uri.query&.include?("v=")
+    elsif uri.host == "youtu.be"
+      uri.path.present? && uri.path != "/"
+    else
+      false
+    end
   end
 
   def self.screenshotable?(post)
