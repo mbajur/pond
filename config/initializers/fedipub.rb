e7853af8504d7c2b69249f618cb9e0d45ad7eb6b
mbajur <mbajur@gmail.com>
2026-08-21 16:37:20 +0200

    Include fedipub host setting

--- config/initializers/fedipub.rb
+++ config/initializers/fedipub.rb
@@ -1,1 +1,8 @@
 Fedipub.config_from "fedipub"
+
+if Rails.env.production?
+  Fedipub.configure do |config|
+    config.site_host = "https://#{Figaro.env.host}"
+    config.open_registrations = false
+  end
+end
