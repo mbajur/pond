bb997c666b3ecb5b375e638ae1c3ff126a82e2b7
mbajur <mbajur@gmail.com>
2026-08-21 16:03:03 +0200

    Disable follow specs

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Follow, type: :model do
+xdescribe Follow, type: :model do
   it { is_expected.to belong_to(:actor).class_name("User") }
   it { is_expected.to belong_to(:target) }
 end
