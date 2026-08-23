3293d6783fccf3ff136bda4158d86ff7087af816
mbajur <mbajur@gmail.com>
2026-08-23 11:21:14 +0100

    Update 20260821174420_move_posts_to_fedipub_actors.rb

--- db/data/20260821174420_move_posts_to_fedipub_actors.rb
+++ db/data/20260821174420_move_posts_to_fedipub_actors.rb
@@ -4,7 +4,7 @@ class MovePostsToFedipubActors < ActiveRecord::Migration[8.1]
   def up
     Post.find_each do |post|
       user = User.find(post.user_id)
-      post.update! fedipub_actor: user
+      post.update! fedipub_actor: user.fedipub_actor
     end
   end
 
